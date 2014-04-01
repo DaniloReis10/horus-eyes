c5f879af76cdf060bd4fa957beb1a331d01bc3b1
Tiago Portela <tiagoporteladesouza@gmail.com>
2014-04-01 14:01:50 -0300

    Modificações nos métodos drawImage

--- geolocator/src/simulagent/MobileAgent.java
+++ geolocator/src/simulagent/MobileAgent.java
@@ -3,95 +3,94 @@ package simulagent;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.util.Iterator;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import location.facade.IGeoPosition;
-import location.facade.IMobileDevice;
-import location.facade.IProximityListener;
-import location.facade.IVisibilityListener;
-import location.geoengine.DevicePath;
 import location.geoengine.GeoPosition;
-
-import trilaceration.Point;
 import trilaceration.ScaleConverter;
 
 /**
  * Classe do agente que possui um interface de rede sem fio
+ * 
  * @author DaniloReis
- *
+ * 
  */
-public class MobileAgent extends RFAgent{
-	private AgentPath                  path;// Rota do agente no dia
-	private boolean                   moved; 
-	private GeoPosition         oldPosition;
+public class MobileAgent extends RFAgent {
+    private AgentPath path;// Rota do agente no dia
 
+    /**
+     * Construtor generico
+     * 
+     */
+    public MobileAgent() {
+        this.path = new AgentPath();
+        this.mobility = AgentMobility.MOBILE;
+    }
+    
+    /**
+     * Construtor com inicialização do ID
+     * @param integer 
+     */
+    public MobileAgent(Integer agentID) {
+        this.path = new AgentPath();
+        this.mobility = AgentMobility.MOBILE;
+        this.id = agentID;
+    }
 
-	/**
-	 * Construtor generico
-	 */
-	public MobileAgent() {
-		super();
-		path     = new AgentPath();
-		moved    = false;
-		mobility = RFAgent.MOBILE;
-		
-	}
-	// Metodos set e gets
-	public AgentPath getPath() {
-		return path;
-	}
+    // Metodos set e gets
+    public AgentPath getPath() {
+        return path;
+    }
 
-	public void setPath(AgentPath path) {
-		this.path = path;
-	}
-	
-	public int getMobility() {
-		return mobility;
-	}
+    public void setPath(AgentPath path) {
+        this.path = path;
+    }
 
-	public void setMobility(int mobility) {
-		this.mobility = mobility;
-	}
+    public AgentMobility getMobility() {
+        return mobility;
+    }
 
-	@Override
-	public void clearImage(BufferedImage image) {
-		Graphics  g;
+    public void setMobility(AgentMobility mobility) {
+        this.mobility = mobility;
+    }
 
-	//	if( moved){
-			g = image.getGraphics();
-			g.setColor(Color.WHITE);
-			g.fillOval(ScaleConverter.convertToX(currentPosition)-5, ScaleConverter.convertToY(currentPosition)-5, 10, 10);	
-	//		moved = false;
-	//	}
-	}
+    @Override
+    public void clearImage(BufferedImage image) {
+        Graphics g;
 
-	@Override
-	public void drawImage(BufferedImage image, Color color) {
-		Graphics  g;
+        g = image.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillOval(ScaleConverter.convertToX(currentPosition) - 5,
+                ScaleConverter.convertToY(currentPosition) - 5, 10, 10);
+    }
 
-		//if(moved){
-			g = image.getGraphics();
-			g.setColor(Color.MAGENTA);
-			g.fillOval(ScaleConverter.convertToX(currentPosition)-5, ScaleConverter.convertToY(currentPosition)-5, 10, 10);		
-			//moved = false;
-		//}
-	}
+//    @Override
+//    public void drawImage(BufferedImage image, Color color) {
+//        Graphics g;
+//
+//        g = image.getGraphics();
+//        g.setColor(Color.MAGENTA);
+//        g.fillOval(ScaleConverter.convertToX(currentPosition) - 5,
+//                ScaleConverter.convertToY(currentPosition) - 5, 10, 10);
+//    }
 
-	@Override
-	public void moveImage() {
-		currentTime = (currentTime) % PathFactory.TICKS_DAY;
-	    oldPosition = currentPosition;
-		currentPosition = (GeoPosition) path.getPositionAtTime(currentTime);
-		currentTime++;
-//		if ((currentPosition.getLatitude()!= oldPosition.getLatitude())||(currentPosition.getLongitude()!= oldPosition.getLongitude())){
-//		    oldPosition = currentPosition;
-//		    moved = true;
-//		}
-	}
-	
-
+    /**
+     * <p>Draws a filled circle representing mobile agents</p>
+     * 
+     * 
+     * @author tiagoportela <tiagoporteladesouza@gmail.com>
+     * @param
+     * @return
+     */
+    @Override
+    public void drawImage(Graphics graphics, Color color) {
+        graphics.setColor(color);
+        graphics.fillOval(ScaleConverter.convertToX(currentPosition) - 5, ScaleConverter.convertToY(currentPosition) - 5, 10, 10);
+    }
+    
+    @Override
+    public void moveImage() {
+        currentTime = (currentTime) % PathFactory.TICKS_DAY;
+        currentPosition = (GeoPosition) path.getPositionAtTime(currentTime);
+        currentTime++;
+    }
 
 }
--- geolocator/src/simulagent/RFAgent.java
+++ geolocator/src/simulagent/RFAgent.java
@@ -4,11 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import trilaceration.ScaleConverter;
-
 import location.facade.IGeoPosition;
 import location.facade.IMobileDevice;
 import location.facade.IProximityListener;
@@ -16,191 +11,207 @@ import location.facade.IVisibilityListener;
 import location.geoengine.DevicePath;
 import location.geoengine.GeoPosition;
 
-public class RFAgent implements IMobileDevice,IActionTick {
-	protected int                    mobility;// Tipo do agente Moével ou fixo 
-	protected GeoPosition     currentPosition;// Posicao corrente
-	protected int                 currentTime;// Tempo corrente em relacao 0:00 em minutos no dia
-    private boolean                    drawed;
-	
-	public static final int  FIXED = 0;// Tipo de agente Fixo
-	public static final int MOBILE = 1;// Tipo de agente Móvel
-	
-	
-	public RFAgent() {
-		// TODO Auto-generated constructor stub
-		drawed   = false;
-		mobility = FIXED;
-	}
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
-	public GeoPosition getCurrentPosition() {
-		return currentPosition;
-	}
+import trilaceration.ScaleConverter;
 
-	public void setCurrentPosition(IGeoPosition currentPosition) {
-		if(currentPosition != null){
-		   this.currentPosition = (GeoPosition) currentPosition;
-		}
-	}
+public class RFAgent implements IMobileDevice, IActionTick {
+    protected AgentMobility mobility;           // Tipo do agente Moével ou fixo
+    protected GeoPosition currentPosition;      // Posicao corrente
+    protected int currentTime;                  // Tempo corrente em relacao 0:00 em minutos no dia
 
-	@Override
-	public Integer getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    protected Integer id;
+    
+    public RFAgent(Integer agentID) {
+        this.mobility = AgentMobility.FIXED;
+        this.id = agentID;
+    }
 
-	@Override
-	public void setGeoPosition(IGeoPosition position) {
-		// TODO Auto-generated method stub
+    /**
+     * 
+     */
+    public RFAgent() {}
 
-	}
+    public GeoPosition getCurrentPosition() {
+        return currentPosition;
+    }
 
-	@Override
-	public IGeoPosition getGeoPosition() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void setCurrentPosition(IGeoPosition currentPosition) {
+        if (currentPosition != null) {
+            this.currentPosition = (GeoPosition) currentPosition;
+        }
+    }
 
-	@Override
-	public double getDistanceFrom(IMobileDevice device) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public Integer getId() {
+        return id;
+    }
 
-	@Override
-	public double getLastDistance() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public void setGeoPosition(IGeoPosition position) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public Integer getType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	@Override
-	public void setType(Integer type) {
-		// TODO Auto-generated method stub
+    @Override
+    public IGeoPosition getGeoPosition() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    @Override
+    public double getDistanceFrom(IMobileDevice device) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public void setDistanceOn(double dist) {
-		// TODO Auto-generated method stub
+    @Override
+    public double getLastDistance() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	}
+    @Override
+    public Integer getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void setGroup(Integer group) {
-		// TODO Auto-generated method stub
+    @Override
+    public void setType(Integer type) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public Integer getGroup() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void setDistanceOn(double dist) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public double getDistanceOn() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    }
 
-	@Override
-	public void setDevicePath(DevicePath path) {
-		// TODO Auto-generated method stub
+    @Override
+    public void setGroup(Integer group) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public DevicePath getDevicePath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Integer getGroup() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void setProximityListener(IProximityListener listener) {
-		// TODO Auto-generated method stub
+    @Override
+    public double getDistanceOn() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	}
+    @Override
+    public void setDevicePath(DevicePath path) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public IProximityListener getProximityListener() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	@Override
-	public IVisibilityListener getVisibilityListener() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public DevicePath getDevicePath() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public String toXML() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void setProximityListener(IProximityListener listener) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void toXML(Element devices, Document doc) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public IProximityListener getProximityListener() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void fromXML(Element item) {
-		// TODO Auto-generated method stub
+    @Override
+    public IVisibilityListener getVisibilityListener() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    @Override
+    public String toXML() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void setId(Integer id) {
-		// TODO Auto-generated method stub
+    @Override
+    public void toXML(Element devices, Document doc) {
+        // TODO Auto-generated method stub
 
-	}
-	/**
-	 * Interface de animaçao do agente
-	 */
-	@Override
-	public void clearImage(BufferedImage image) {
-		Graphics  g;
+    }
 
-		//if( !drawed){
-			g = image.getGraphics();
-			g.setColor(Color.WHITE);
-			g.fillRect(ScaleConverter.convertToX(currentPosition)-5, ScaleConverter.convertToY(currentPosition)-5, 10, 10);		
-		//	drawed = true;
-		//}
-	}
+    @Override
+    public void fromXML(Element item) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void drawImage(BufferedImage image, Color color) {
-		Graphics  g;
+    }
 
-		//if( !drawed){
-			g = image.getGraphics();
-			g.setColor(Color.GREEN);
-			g.fillRect(ScaleConverter.convertToX(currentPosition)-5, ScaleConverter.convertToY(currentPosition)-5, 10, 10);		
-			//drawed = true;
-		//}
-	}
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	@Override
-	public void moveImage() {
-		currentTime = (currentTime) % PathFactory.TICKS_DAY;
-		currentTime++;
-	}
+    /**
+     * Interface de animaçao do agente
+     */
+    @Override
+    public void clearImage(BufferedImage image) {
+        Graphics g;
 
-	@Override
-	public void startListener() {
+        g = image.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(ScaleConverter.convertToX(currentPosition) - 5, ScaleConverter.convertToY(currentPosition) - 5, 10, 10);
+    }
 
-	}
+//    @Override
+//    public void drawImage(BufferedImage image, Color color) {
+//        Graphics g;
+//
+//        g = image.getGraphics();
+//        g.setColor(Color.GREEN);
+//        g.fillRect(ScaleConverter.convertToX(currentPosition) - 5, ScaleConverter.convertToY(currentPosition) - 5, 10, 10);
+//    }
 
-	@Override
-	public void stopListener() {
-	}
+    @Override
+    public void moveImage() {
+        currentTime = (currentTime) % PathFactory.TICKS_DAY;
+        currentTime++;
+    }
 
+    @Override
+    public void startListener() {
+
+    }
+
+    @Override
+    public void stopListener() {
+    }
+
+    public AgentMobility getMobility() {
+        return mobility;
+    }
+
+    /**
+     * <p>Draws a rect representing fixed agents</p>
+     * 
+     * 
+     * @author tiagoportela <tiagoporteladesouza@gmail.com>
+     * @param
+     * @return
+     */
+    @Override
+    public void drawImage(Graphics graphics, Color color) {
+        graphics.setColor(Color.RED); // COR ESPECÍFICA PARA REPRESENTAR AGENTE FIXO
+        graphics.fillRect(ScaleConverter.convertToX(currentPosition) - 5, ScaleConverter.convertToY(currentPosition) - 5, 10, 10);
+    }
 }
